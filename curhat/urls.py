6d63016e1285e5cf7b3a486fdf2921980a9c0956
fahiradh <fahiraadindiah16@gmail.com>
2022-10-28 00:42:29 +0700

    fix urls.py

--- curhat/urls.py
+++ curhat/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.curhat, name='curhat'),
     path('success/', views.success, name='success'),
     path('json/', views.riwayat_json, name='riwayat-json'),
+    path('riwayat-konsultasi/', views.show_riwayat, name='riwayat-konsultasi')
 ]
