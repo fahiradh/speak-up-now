32498efd776fbec39b14d0cb237b120f7f96d6b6
fahiradh <fahiraadindiah16@gmail.com>
2022-10-28 23:00:21 +0700

    add views.py

--- curhat/views.py
+++ curhat/views.py
@@ -1,53 +1,58 @@
+import datetime
+from pyexpat import model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.core import serializers
-from django.http import HttpResponse
-from . import forms, models
+from django.http import HttpResponse, JsonResponse
+from . import models
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-def curhat(request):
-    post_form = forms.curhatForm(request.POST or None)
+@csrf_exempt
+def add(request):
     if request.method == "POST":
-        if post_form.is_valid():
-            curhatData = models.curhatDong(
-                date = post_form.data['date_year'] + "-" + post_form.data['date_month'] + "-" + post_form.data['date_day'],
-                name = post_form.data['name'],
-                title = post_form.data['title'],
-                description = post_form.data['description'],
-                contactable = post_form.data['contactable'],
-            )
-            curhatData.save()
-    else:
-        post_form = forms.curhatForm()
-            
-    contexts = {
-        'curhat': models.curhatDong.objects.all().values(),
-        'form': post_form,
-    }
-    return render(request, 'curhat.html', contexts)
+        name = request.POST.get("name")
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        contactable = request.POST.get("contactable")
+        obj = models.curhatDong.objects.create(
+            date=datetime.date.today(),
+            name=name,
+            title=title,
+            description=description,
+            contactable=contactable,
+        )
+        result = {
+            'fields':{
+                'date': obj.date,
+                'name': obj.name,
+                'title': obj.title,
+                'description': obj.description,
+                'contactable': obj.contactable,
+            },
+            'pk': obj.pk
+        }
+    return JsonResponse(result)
 
 def show_riwayat(request):    
     # riwayat = models.curhatDong.objects.filter(user=request.user)    
-    riwayat = models.curhatDong.objects.all().values(),
+    riwayat = models.curhatDong.objects.all()
     contexts = {
         'list': riwayat,
-        # 'curhat': models.curhatDong.objects.all().values(),
         # 'username' : request.user.username,
     }
     return render(request, 'riwayat-konsultasi.html', contexts)
 
-# @login_required(login_url='login/')
-# def riwayat_json(request):
-#     # riwayat = models.curhatDong.objects.filter(user=request.user)
-#     riwayat = models.curhatDong.objects.all(),
-#     return HttpResponse(serializers.serialize('json', riwayat), content_type='application/json')
-
 def riwayat_json(request):
     riwayat = models.curhatDong.objects.all()
-    serialized_posts = serializers.serialize('json', riwayat)
-    print(serialized_posts)
-    context = {'riwayat': riwayat, 'serialized_posts': serialized_posts}
-    return render(request, 'riwayat-konsultasi.html', context)
+    # serialized_posts = serializers.serialize('json', riwayat)
+    # print(serialized_posts)
+    # context = {'riwayat': riwayat, 'serialized_posts': serialized_posts}
+    return HttpResponse(serializers.serialize("json", riwayat), content_type="application/json")
+    # return render(request, 'riwayat-konsultasi.html', context)
 
-def success(request):
-    return render(request, 'success.html')
+def delete_konsultasi(request, id):
+    # task = models.curhatDong.objects.filter(id=id, user=request.user)
+    task = models.curhatDong.objects.filter(id=id)
+    task.delete()
+    return HttpResponse()
