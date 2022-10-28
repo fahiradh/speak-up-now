c3fb7a3bdd1c0ed25c4ee9312fa94330c7a9b383
fahiradh <fahiraadindiah16@gmail.com>
2022-10-28 22:59:44 +0700

    add urls.py

--- curhat/urls.py
+++ curhat/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.curhat, name='curhat'),
-    path('success/', views.success, name='success'),
+    # path('', views.show_form, name='show-form'),
+    path('add/', views.add, name='add'),
     path('json/', views.riwayat_json, name='riwayat-json'),
-    path('riwayat-konsultasi/', views.show_riwayat, name='riwayat-konsultasi')
+    path('', views.show_riwayat, name='konsultasi'),
+    path('delete/<int:id>', views.delete_konsultasi, name='delete'),
 ]
