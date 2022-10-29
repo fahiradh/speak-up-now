f6d9c751be969c69af04e40299d66085a1a126a7
Ikhlasul Akmal Hanif <2112akmal@gmail.com>
2022-10-29 21:08:44 +0700

    add user page

--- project_django/urls.py
+++ project_django/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from curhat import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
