83e3763057c6f8b4c87c8a199d572d58850d825e
fahmisf19 <fahmisfatalnuck@gmail.com>
2022-11-01 00:00:48 +0700

    fixing login and register page

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,4 @@
-from home.views import homepage, register, login
+from home.views import homepage, register, login_user
 from django.urls import path
 
 app_name = 'home'
@@ -6,5 +6,5 @@ app_name = 'home'
 urlpatterns= [
     path('', homepage, name='homepage'),
     path('register/', register, name='register'),
-    path('login/', login, name='login'),
+    path('login/', login_user, name='login'),
 ]
