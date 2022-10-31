b1361083b054948f3328b7b2a4c808e6a1c61367
fahmisf19 <fahmisfatalnuck@gmail.com>
2022-10-31 14:45:40 +0700

    design login and register page

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,4 @@
-from home.views import homepage, register
+from home.views import homepage, register, login
 from django.urls import path
 
 app_name = 'home'
@@ -6,4 +6,5 @@ app_name = 'home'
 urlpatterns= [
     path('', homepage, name='homepage'),
     path('register/', register, name='register'),
+    path('login/', login, name='login'),
 ]
