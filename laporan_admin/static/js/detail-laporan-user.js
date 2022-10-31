88a05d0ed6399d50c9a02c4e62798a0542f920f7
fahmisf19 <fahmisfatalnuck@gmail.com>
2022-10-31 14:24:40 +0700

    fix delete bug

---
+++ laporan_admin/static/js/detail-laporan-user.js
@@ -0,0 +1,41 @@
+function showContent() {
+    $.get(`/laporan_admin/json/${pk}`, function(data){
+    let content = '';
+    content += `
+    <div class="top-section d-flex flex-row justify-content-between">
+        <h3>${data.fields.case_name}</h3>
+        <input class="btn btn-primary btn-reply" data-bs-toggle="modal" data-bs-target="#exampleModal" type="button" value="Reply">
+    </div>
+    <p style="margin-bottom: 3rem;">${data.fields.crime_place}</p>
+    <p><b>Informer: </b><br>
+        ${data.fields.name}</p>
+    <p><b>Contact Person: </b><br>
+        ${data.fields.phone_num}/${data.fields.email}</p>
+    <p><b>Victim: </b><br>
+        ${data.fields.victim_name}</p>
+    <p><b>VICTIM DESCRIPTION</b></p>
+    <p>${data.fields.victim_description}</p>
+    <p><b>CHRONOLOGY</b></p>
+    <p>${data.fields.chronology}</p>
+    `;
+    $('.content').html(content);
+})
+}
+
+$(document).on('submit', '#form_reply', function(e) {
+    e.preventDefault()
+    $.ajax({
+        type:"POST",
+        url:"{% url 'laporan_admin:reply_laporan_user' %}",
+        data:{
+            status_case:$("#status").val(),
+            admin_response:$("#message").val(), 
+        },
+        dataType: 'json',
+    })
+    document.getElementById("form_reply").reset()
+})
+
+$(document).ready( function(){
+  $.get(`/laporan_admin/json/${pk}`, showContent);
+})
--- project_django/urls.py
+++ project_django/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('curhat-admin/', include('curhat_admin.urls')),
     path('user-page/', include('user_page.urls')),
     path('home/', include('home.urls')),
+    path('laporan-admin/', include('laporan_admin.urls'))
 ]
 
