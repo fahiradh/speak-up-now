39a4f8436189437e09869c7404251c124eec89db
Ikhlasul Akmal Hanif <2112akmal@gmail.com>
2022-11-02 17:19:46 +0700

    login-logout navbar

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,4 @@
-from home.views import homepage, register, login_user
+from home.views import homepage, register, login_user,logout_user
 from django.urls import path
 
 app_name = 'home'
@@ -7,4 +7,5 @@ urlpatterns= [
     path('', homepage, name='homepage'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout')
 ]
