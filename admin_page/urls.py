3dca5e0c211f261125b0c99833559f0cae76d31e
fahmisf19 <fahmisfatalnuck@gmail.com>
2022-11-02 22:08:36 +0700

    fix redirect login admin and responsive page

--- admin_page/urls.py
+++ admin_page/urls.py
@@ -4,6 +4,6 @@ from admin_page.views import show_admin_page
 app_name = 'user_page'
 
 urlpatterns = [
-    path('', show_admin_page, name='show_user_page'),
+    path('', show_admin_page, name='show_admin_page'),
 
 ]
