8b7ab25783565a27d264e8abec2a6b2f52b75a71
fahmisf19 <fahmisfatalnuck@gmail.com>
2022-11-02 10:13:50 +0700

    add date to reply

--- curhat_admin/models.py
+++ curhat_admin/models.py
@@ -1,9 +1,8 @@
-from email.policy import default
 from django.db import models
-import datetime
+from django.utils import timezone
 
 # Create your models here.
 class curhatAdmin(models.Model):
-    date = models.DateField(default = datetime.date.today)
+    date = models.DateField()
     title = models.CharField(max_length = 100)
     description = models.CharField(max_length = 500)
