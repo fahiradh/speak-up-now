48a20f542674f08702c75ffc673a5dd3b2358935
Fahmi Sabila Firdaus <73156219+fahmisf19@users.noreply.github.com>
2022-12-10 20:32:19 +0700

    Update urls.py

--- curhat_admin/urls.py
+++ curhat_admin/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from curhat_admin.views import show_table_curhat, show_curhat_details, table_json, delete_json, reply_json, add_reply, add_reply_flutter
+from curhat_admin.views import show_table_curhat, show_curhat_details, table_json, delete_json, reply_json, add_reply, add_reply_flutter, delete_json_flutter
 app_name = 'curhat_admin'
 
 urlpatterns = [
@@ -12,4 +12,5 @@ urlpatterns = [
     path('json', table_json, name='table-json'),
     path('delete/<int:i>', delete_json, name='delete-json'),
     path('add-reply-flutter', add_reply_flutter, name='add-reply-flutter'),
+    path('delete-flutter/<int:i>', delete_json_flutter, name='delete-json-flutter'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
