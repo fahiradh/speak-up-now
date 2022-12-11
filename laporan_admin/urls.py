1b99ec6ff325c82f96ad49e0dfe13f3f67c3b0c6
alifianadp <alifianadp@gmail.com>
2022-12-12 00:52:59 +0700

    Add Flutter Views

--- laporan_admin/urls.py
+++ laporan_admin/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from laporan_admin.views import show_laporan_user, show_detail_laporan, add_response, show_response_json, show_laporan_json, delete_laporan
+from laporan_admin.views import show_laporan_user, show_detail_laporan, add_response, show_response_json, show_laporan_json, delete_laporan, response_json, add_response_flutter, delete_laporan_flutter
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -12,4 +12,7 @@ urlpatterns = [
     path('json/', show_laporan_json, name='show_laporan_json'),
     path('response/json/', show_response_json, name='show_response_json'),
     path('delete/<int:id>', delete_laporan, name='delete_laporan'),
+    path('response/<int:id>', response_json, name='response_json'),
+    path('add-response-flutter/', add_response_flutter, name='add_response_flutter'),
+    path('delete-flutter/<int:id>', delete_laporan_flutter, name='delete_laporan_flutter'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
