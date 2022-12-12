4ac68cc164521825c18cb3a925486a69a9fa553d
Muhammad Satria Ramadhan <rama081982@gmail.com>
2022-12-12 23:01:58 +0700

    add logout

--- home/urls.py
+++ home/urls.py
@@ -11,4 +11,5 @@ urlpatterns= [
     path('register/validate-username/', validate_username, name='validate-username'),
     path('user-details/', userdetail, name='userdetails'),
     path('register-ajax/', register_ajax, name='registerajax'),
+    path('logout-ajax/', logout_flutter, name='logoutajax')
 ]
